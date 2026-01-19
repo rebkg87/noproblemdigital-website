4fc9d770204d94a52c37f44d2daa925447ed100e
Rebeca Garcia <rebe.ga87@gmail.com>
2026-01-19 23:37:29 +0100

    fix(toggle): change toggle button position for darkMode

--- src/componets/ui/ThemeToggle.jsx
+++ src/componets/ui/ThemeToggle.jsx
@@ -4,7 +4,7 @@ export const ThemeToggle = () => {
     const { darkMode, toggleDarkMode } = useTheme()
     return (
         <button onClick={toggleDarkMode} aria-label="Toggle theme" className=" relative w-13.75 h-8.5 rounded-full border-7 border-brand-primary transition-colors">
-            <span className={`absolute -translate-y-1/2 w-3.75 h-3.75 rounded-full transition-all ${darkMode ? 'right-1 bg-brand-secondary' : 'left-1 bg-brand-primary'}`}/>
+            <span className={`absolute -translate-y-1/2 w-3.75 h-3.75 rounded-full transition-all ${darkMode ? 'left-1 bg-brand-primary' : 'right-1 bg-brand-secondary' }`}/>
         </button>
     )
 }
