bc9d7dadf9f3fe1532bdcf88e85cfc14c8f69cd1
Rebeca Garcia <rebe.ga87@gmail.com>
2026-01-19 13:27:24 +0100

    feat(setup): add project base setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/globals.css'
+import { ThemeProvider } from './providers/ThemeProvider'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <RouterProvider router={router}/>
+    </ThemeProvider>
   </StrictMode>,
 )
