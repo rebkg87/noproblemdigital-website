39db79525421a200ca822e71173d25e9d2654a6e
Rebeca Garcia <rebe.ga87@gmail.com>
2026-01-20 14:59:07 +0100

    feat(marquee): add services banner marquee with animation

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,17 @@
+import { useTranslation } from "react-i18next"
+import { ScrollVelocityMarquee } from "../../componets/ui/ScrollVelocityMarquee"
 import { HomepageHeroSection } from "./sections/HomepageHeroSection"
 
 export const Homepage = () => {
+    const {t} = useTranslation()
+    const words = t ("services.rotating", { returnObjects: true });
     return (
         <div>
             <HomepageHeroSection/>
+            <div className="w-full h-line-h1 bg-brand-primary justify-center flex items-center overflow-hidden">
+                <ScrollVelocityMarquee texts={[words.join('\u00A0\u00A0\u00A0|\u00A0\u00A0\u00A0')]} velocity={100} className="custom-scroll-text text-content-inverse"
+                />
+            </div>
         </div>
     )
 }
