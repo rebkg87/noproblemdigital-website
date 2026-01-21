4f31659cddc165ed6eeb5711915a02a38d37a04c
Rebeca Garcia <rebe.ga87@gmail.com>
2026-01-21 11:06:47 +0100

    feat(components): add variables to textSection component

--- src/pages/homepage/sections/HomepageHeroSection.jsx
+++ src/pages/homepage/sections/HomepageHeroSection.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import bgHero from '../../../assets/images/bg-hero.png'
 import { Button } from '../../../componets/ui/Button'
-import { TextSection } from './components/TextSection'
+import { TextSection } from '../../../componets/ui/TextSection'
 
 export const HomepageHeroSection = () => {
     const { t } = useTranslation();
@@ -12,11 +12,8 @@ export const HomepageHeroSection = () => {
             style={{ backgroundImage: `url(${bgHero})` }}
         >
             <div className='pt-66 flex flex-col items-center justify-center gap-14.25'>
-                <TextSection/>
-                <Button 
-                    buttonStyle="bg-brand-secondary  hover:bg-state-hover-accent  focus:border-brand-secondary text-white"
-                    buttonText={buttonText}
-                    />
+                <TextSection isAnimated titleKey='hero.title' subtitleKey='hero.subtitle' variant='hero'/>
+                <Button variant='primary'>{buttonText}</Button>
             </div>
         </section>
     )
