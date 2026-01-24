5a72180b9adbce9ad85248c63cbba176621ea7b4
Rebeca Garcia <rebe.ga87@gmail.com>
2026-01-24 12:05:32 +0100

    feat(form): update form component

--- src/componets/ui/Checkbox.jsx
+++ src/componets/ui/Checkbox.jsx
@@ -5,9 +5,9 @@ export const Checkbox = ({
 
     return (
         <div>
-            <label htmlFor={checkboxId} className={`flex items-center gap-4 select-none ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}`}>
-                <input id={checkboxId} name={name} type="checkbox" checked={checked} onChange={onChange} disabled={disabled} required={required} className="h-5 w-5 rounded-[5px] border-2 border-content-secondary bg-gray-100 checked:bg-brand-secondary checked:accent-brand-primary focus:outline-none active:outline-none" />
-                <span className={`text-size-l3 text-content-primary ${labelClassName}`}> {label}{required && <span className="text-brand-secondary ml-1">*</span>}
+            <label htmlFor={checkboxId} className={`flex items-center gap-2 select-none ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}`}>
+                <input id={checkboxId} name={name} type="checkbox" checked={checked} onChange={onChange} disabled={disabled} required={required} className="appearance-none h-5 w-5 rounded-[5px] border-2 border-content-secondary bg-white checked:bg-brand-secondary checked:border-brand-secondary focus:outline-none cursor-pointer relative checked:after:content-['✓'] checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white checked:after:text-xs checked:after:font-bold" />
+                <span className={`text-size-l3 text-content-primary ${labelClassName}`}> {label}{required && <span className="text-brand-secondary max-h-125">*</span>}
                 </span>
             </label>
         </div>
--- src/componets/ui/Form.jsx
+++ src/componets/ui/Form.jsx
@@ -7,11 +7,11 @@ import { Checkbox } from "./Checkbox"
 export const Form = ({ content }) => {
     return (
         <form action="">
-            <div className="flex flex-col w-auto gap-3.5 ">
+            <div className="flex flex-col w-auto gap-3.5 bg-white px-16.5 py-16 rounded-[20px] shadow-soft">
                 <div className="flex flex-col w-auto gap-2.5  ">
                     {content.form.map((field, index) => (
                         field.type === "textarea" ? (
-                            <TextArea key={index} label={field.label} id={field.id} placeholder={field.placeholder}/>
+                            <TextArea key={index} label={field.label} id={field.id} placeholder={field.placeholder} className="min-h-41.25"/>
                         ) : (
                             <Input key={index} label={field.label} type={field.type} id={field.id} placeholder={field.placeholder} />
                         )
